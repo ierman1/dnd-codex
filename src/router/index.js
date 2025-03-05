c292e2d99975aa8ed125fb43b256ffcd82f21ada
ierman1 <germanperezb98@gmail.com>
2025-03-05 19:55:37 +0100

    First commit

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import CharacterView from '@/views/CharacterView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: CharacterView,
+    },
+  ],
+})
+
+export default router
---
+++ src/views/CharacterView.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+  name: 'CharacterView',
+}
+</script>
+<template>
+  <h1>Character</h1>
+</template>
