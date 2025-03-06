f5551f77ed29e3cd525607c97235ac5256dec78a
ierman1 <germanperezb98@gmail.com>
2025-03-06 13:39:35 +0100

    Adds graphql apollo client

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, h, provide } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { InMemoryCache, ApolloClient, createHttpLink } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
-const app = createApp(App)
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://www.dnd5eapi.co/graphql/2014',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
 
 app.use(router)
 
