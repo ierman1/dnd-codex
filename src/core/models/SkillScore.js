3cc5ed89e41ec63924c20d489cbb9cf1bb82532f
ierman1 <germanperezb98@gmail.com>
2025-03-09 19:38:23 +0100

    Adds expertise to the skill model

--- src/core/models/SkillScore.js
+++ src/core/models/SkillScore.js
@@ -1,6 +1,45 @@
 export default class SkillScore {
-  constructor(name, proficency = false) {
-    this.name = name
-    this.proficency = proficency
+  constructor(name, proficency = false, expertise = false) {
+    this._name = name
+    this._proficency = proficency
+    this._expertise = expertise
+  }
+
+  /**
+   * Skill name
+   */
+
+  get name() {
+    return this._name
+  }
+
+  set name(name) {
+    this._name = name
+  }
+
+  /**
+   * Skill proficency
+   */
+
+  get proficency() {
+    return this._proficency
+  }
+
+  set proficency(proficency) {
+    this._proficency = proficency
+  }
+
+  /**
+   * Skill expertise
+   */
+
+  get expertise() {
+    return this._expertise
+  }
+
+  set expertise(expertise) {
+    if (expertise && !this.proficency) this.proficency = true
+
+    this._expertise = expertise
   }
 }
