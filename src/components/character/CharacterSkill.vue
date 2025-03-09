380e9fc11fd375d090a172dcb20186c0008a8892
ierman1 <germanperezb98@gmail.com>
2025-03-09 20:57:17 +0100

    Applies the refactoring on the models to the components

---
+++ src/components/character/CharacterSkill.vue
@@ -0,0 +1,37 @@
+<script>
+import { computed, inject } from 'vue'
+import ScoreModifier from './ScoreModifier.vue'
+
+export default {
+  name: 'CharacterSkill',
+  components: { ScoreModifier },
+  props: {
+    skillName: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const character = inject('character')
+    const skill = character.findSkill(props.skillName)
+
+    const hasProficency = computed(() => skill.proficency)
+    const hasExpertise = computed(() => skill.expertise)
+
+    return {
+      character,
+      skill,
+      hasProficency,
+      hasExpertise,
+    }
+  },
+}
+</script>
+<template>
+  <div v-if="skill">
+    <input :disabled="hasExpertise" type="checkbox" v-model="skill.proficency" />
+    <input :disabled="!hasProficency" type="checkbox" v-model="skill.expertise" />
+    <ScoreModifier :number="skill.modifier" />
+    <span class="inline-block ml-2">{{ skill.name }}</span>
+  </div>
+</template>
--- src/components/character/CharacterSkillList.vue
+++ src/components/character/CharacterSkillList.vue
@@ -1,10 +1,10 @@
 <script>
 import { inject } from 'vue'
-import ScoreModifier from './ScoreModifier.vue'
+import CharacterSkill from './CharacterSkill.vue'
 
 export default {
   name: 'CharacterSkillList',
-  components: { ScoreModifier },
+  components: { CharacterSkill },
   props: {
     stat: {
       type: Object,
@@ -16,20 +16,9 @@ export default {
     const skillNames = props.stat.constructor.skillsRelated()
     const skills = character.skills.filter((skill) => skillNames.includes(skill.name))
 
-    const findSkillIndex = (skillName) =>
-      character.skills.findIndex((skill) => skill.name === skillName)
-
-    const skillModifier = (skillName) => {
-      const proficency = character.skills[findSkillIndex(skillName)].proficency
-
-      return props.stat.modifier + (proficency ? character.proficencyBonus : 0)
-    }
-
     return {
       character,
       skills,
-      findSkillIndex,
-      skillModifier,
     }
   },
 }
@@ -37,12 +26,6 @@ export default {
 <template>
   <div class="py-3" v-if="skills.length > 0">
     <span class="font-bold">Skills related:</span>
-    <ul>
-      <li v-for="skill in skills" :key="skill.name">
-        <input type="checkbox" v-model="character.skills[findSkillIndex(skill.name)].proficency" />
-        <ScoreModifier :number="skillModifier(skill.name)" />
-        <span class="inline-block ml-2">{{ skill.name }}</span>
-      </li>
-    </ul>
+    <CharacterSkill v-for="skill in skills" :key="skill.name" :skill-name="skill.name" />
   </div>
 </template>
